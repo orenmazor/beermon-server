4bcdd22f5bfd4e945d416f3228f95ffb9dcb900d
Chris Saunders <chris.saunders@shopify.com>
2012-10-18 15:45:30 +0000

    oops

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,9 +1,20 @@
 class RatingsController < ApplicationController
   respond_to :html, :json, :only => [:create]
+  before_filter :find_beer
+
+  def new
+    @rating = @beer.ratings.new
+  end
+
 
   def create
-    beer = Beer.find(params[:beer_id])
     rating = request.format.json? ? @beer.ratings.create(params[:rating]) : nil
     respond_with(rating)
+  end
+
+  private
+
+  def find_beer
+    @beer = Beef.find(params[:beer_id])
   end
 end
