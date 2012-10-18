98dbc06adf0ba805c3edf2197f0573078d73cbe5
Chris Saunders <chris.saunders@shopify.com>
2012-10-18 01:06:15 +0000

    Added super secret passwords

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  http_basic_authenticate_with :name => '05f8a4e34b57482ad021cfb0', :password => 'd84c1dfb42c2abcf0c90180e'
 end
