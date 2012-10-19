afdb4df235f25142e1f38ba3db06a3180ea7385f
Chris Saunders <chris.saunders@shopify.com>
2012-10-19 17:36:27 +0000

    stuff?

---
+++ app/controllers/callback_controller.rb
@@ -0,0 +1,5 @@
+class CallbackController < ApplicationController
+
+  def index
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Beermon::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  Beermon::Application.application_url = "http://beermon.herokuapp.com"
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
