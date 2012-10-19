0d3dad078f197114a882c25c11f163bf5995f3e8
Chris Saunders <chris.saunders@shopify.com>
2012-10-19 17:48:30 +0000

    sending phone calls?

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -1,5 +1,12 @@
 class CallbackController < ApplicationController
 
   def index
+    brewery = Brewery.find_by_phone(params[:phone])
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say "Connecting you to #{brewery.name}", :voice => 'woman'
+      r.Dial :callerId => brewery.phone
+    end
+
+    render :text => response.text, :content_type => "text"
   end
 end
