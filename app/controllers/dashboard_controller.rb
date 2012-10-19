dcc82e95c976c167166448ee11de9f9c8a13d9ec
Aaron Olson <aaron.olson@jadedpixel.com>
2012-10-19 15:05:09 -0400

    Recommended bog water

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,7 +7,7 @@ class DashboardController < ApplicationController
 	def index
     @beer_taps = BeerTap.all
 		@beers = Beer.all
-    recommend_a_beer
+    puts "RECOMMENDED: #{recommend_a_beer.name}"
 	end
 
   def call
@@ -35,21 +35,20 @@ class DashboardController < ApplicationController
     unless current_user.present?
       redirect_to login_url
     end
+  end
 
   def recommend_a_beer
     sorted = @beers.each.map { |beer| [beer, sort_value(beer)] }
-    recommended_beer = sorted.sort_by { |beersort| beersort[1] }.last
+    recommended_beer = sorted.sort_by { |beersort| beersort[1] }.last[0]
   end
 
   def sort_value(beer)
     # upvotes
-    puts "UPVOTES: #{beer.votes.where(:value => 1).inspect}"
     a   = beer.votes.where(:value => 1)
           .map { |v| Math.exp(-FORGET_FACTOR * (Date.today - v.updated_at.to_date).to_i) }
           .reduce(1, :+) * 1.0
 
     # downvotes
-    puts "DOWNVOTES: #{beer.votes.where(:value => -1)}"
     b = beer.votes.where(:value => -1)
           .map { |v| Math.exp(-FORGET_FACTOR * (Date.today - v.updated_at.to_date).to_i) }
           .reduce(1, :+) * 1.0
