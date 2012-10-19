7930b72e2bf5f3112d624dd7d6a7d32afb19e4f0
Chris Saunders <chris.saunders@shopify.com>
2012-10-19 19:53:51 +0000

    dafasf

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -13,7 +13,7 @@ class MeasurementsController < ApplicationController
 
   def create
     measurement_params = ActiveSupport::JSON.decode(params[:measurement]).reject{|k, v| v.blank?}
-    Rails.logger.info("Fancy things: " + measurement_params)
+    Rails.logger.info(measurement_params)
     measurement = @keg.measurements.create(measurement_params)
     respond_with([@keg, measurement])
   end
